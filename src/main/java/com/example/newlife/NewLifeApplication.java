d2f6977d9faf60987d78df315b7e77b3ad1c0b78
abihilary <abihilary@gmail.com>
2025-07-03 21:22:41 +0100

    we getting ther

---
+++ src/main/java/com/example/newlife/NewLifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.newlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newlife/NewLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
