574a9c847fecbb4b6825b2ae8304d44d92b128d4
abihilary <abihilary@gmail.com>
2025-07-08 14:22:23 +0100

    new update

--- src/main/java/com/example/newlife/Controller/Controller.java
+++ src/main/java/com/example/newlife/Controller/Controller.java
@@ -17,7 +17,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-@CrossOrigin(origins = "*")
+
 @RestController
 @RequestMapping("/app")
 public class Controller {
